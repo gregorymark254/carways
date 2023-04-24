363c8c201b49a52894e7b23a2552962edcd4a575
gregorymark254@gmail.com <gregorymark254@gmail.com>
2023-04-24 19:07:31 +0300

    updates

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -72,12 +72,12 @@ const Nav = () => {
                 </a>
                 <ul className="absolute w-full z-[1000] float-left  hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block" aria-labelledby="dropdownMenuButton2"data-te-dropdown-menu-ref>
                   <li>
-                    <a className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
+                    <a className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100  "
                       href="/profile"
                       data-te-dropdown-item-ref>My profile</a>
                   </li>
                   <li>
-                    <a className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
+                    <a className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100"
                       href="#logout"
                       onClick={signOut}
                       data-te-dropdown-item-ref>Log out</a>
