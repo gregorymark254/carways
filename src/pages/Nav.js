cb90d288ed37214c12ee344aa805368f103c88b8
gregorymark254 <gregorymark254@gmail.com>
2023-04-25 22:05:44 +0300

    nav responsive

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -28,37 +28,39 @@ const Nav = () => {
       <div className="bg-white shadow shadow-neutral-700">
         <div className="container mx-auto py-4">
           <ToastContainer />
-          <nav className='flex justify-between items-center px-4'>
+          <nav className='flex justify-around items-center px-4'>
             {/* Top Nav */}
-            <div>
+            <div className="p-2">
               <img className="h-10" src="https://live.themewild.com/carway/assets/img/logo/logo.png" alt="LOGO" />
             </div>
-            <ul className='hidden md:flex text-xl'>
-              <li>
-                <a href="/" className="hover:text-[#4F5DEC] px-3 py-2 ">Home</a>
-              </li>
-              <li>
-                <a href="/cars" className="hover:text-[#4F5DEC] px-3 py-2 ">Cars</a>
-              </li>
-              <li>
-                <a href="/about" className="hover:text-[#4F5DEC] px-3 py-2 ">About</a>
-              </li>
-              <li>
-                <a href="/services" className="hover:text-[#4F5DEC] px-3 py-2 ">Services</a>
-              </li>
-              <li>
-                <a href="/contact" className="hover:text-[#4F5DEC] px-3 py-2">Contact</a>
-              </li>
-            </ul>
-            <div className="md:flex hidden items-center space-x-8">
-              <div className="flex items-center space-x-4">
+            <div>
+              <ul className='hidden md:flex text-xl p-2'>
+                <li>
+                  <a href="/" className="hover:text-[#4F5DEC] px-3 py-2 ">Home</a>
+                </li>
+                <li>
+                  <a href="/cars" className="hover:text-[#4F5DEC] px-3 py-2 ">Cars</a>
+                </li>
+                <li>
+                  <a href="/about" className="hover:text-[#4F5DEC] px-3 py-2 ">About</a>
+                </li>
+                <li>
+                  <a href="/services" className="hover:text-[#4F5DEC] px-3 py-2 ">Services</a>
+                </li>
+                <li>
+                  <a href="/contact" className="hover:text-[#4F5DEC] px-3 py-2">Contact</a>
+                </li>
+              </ul>
+            </div>
+            <div className="flex items-center space-x-8">
+              <div className="flex items-center space-x-4 p-2" id="links">
                 <h3 className="bg-[#4F5DEC] text-white rounded-lg p-2"><FiPhoneCall/></h3>
-                <div>
+                <div className="p-1">
                   <p>Need Car Rent?</p>
-                  <p className="text-[#4F5DEC] font-bold text-lg">+254 702 589 522</p>
+                  <p className="text-[#4F5DEC] font-bold text-lg">+254 798 551 178</p>
                 </div>
               </div>
-              <a href="/cars" className="bg-[#4F5DEC] text-white py-3 px-5 rounded-lg">Explore</a>
+              <a id="links" href="/cars" className="bg-[#4F5DEC] text-white py-3 px-5 rounded-lg">Explore</a>
               {userInfo ? (
                 <div className="relative" data-te-dropdown-ref>
                   <a className="flex items-center  px-3 py-2  transition duration-150 ease-in-out "
@@ -104,7 +106,7 @@ const Nav = () => {
               </svg>
             )}
             {navIsShown && (
-              <div className='md:hidden absolute z-10 top-12 left-0  w-full  text-black'>
+              <div className='md:hidden absolute z-10 top-12 left-0  w-full text-black'>
                 <ul className='text-center mb-2 bg-white p-2 mx-4'>
                   <li>
                     <a href="/" className="px-3 text-lg hover:text-blue-600"><b>Home</b></a>
@@ -122,9 +124,15 @@ const Nav = () => {
                     <a href="/contact" className="px-3 py-2 text-lg hover:text-blue-600"><b>Contact</b></a>
                   </li>
                   <hr />
-                  <li className="p-2">
-                    <a href="/login" className="px-3 py-2 text-lg bg-[#4F5DEC] text-white rounded-lg hover:bg-blue-400"><b>Login</b></a>
-                  </li>
+                  {userInfo ? (
+                    <li className="p-2">
+                      <a href="#logout" onClick={signOut} className="px-3 py-2 text-lg bg-[#252c79] text-white rounded-lg hover:bg-blue-400"><b>Logout</b></a>
+                    </li>
+                  ) : (
+                    <li className="p-2">
+                      <a href="/login" className="px-3 py-2 text-lg bg-[#4F5DEC] text-white rounded-lg hover:bg-blue-400"><b>Login</b></a>
+                    </li>
+                  )}
                 </ul>
               </div>
             )}
